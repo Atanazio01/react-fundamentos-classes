db4a97f6814dc2cfc0cf9868e8ffb50a294ab597
Atanazio01 <marcosd.atanazio@gmail.com>
2021-11-11 14:51:56 -0300

    Converting the ThemeProvider for ClassComponent

--- src/App.js
+++ src/App.js
@@ -7,54 +7,14 @@ import { ThemeProvider, ThemeContext } from './contexts/ThemeContext';
 
 import themes from './styles/themes';
 class App extends React.Component {
-  state = {
-    changed: false,
-  };
-
-  // Executa quando o componente é montado
-  componentDidMount() {
-    console.log('componentDidMount executed');
-  }
-
-  // Executa após a rerenderização do componente
-  componentDidUpdate(prevProps, prevState) {
-    console.log('componentDidUpdate ', {
-      currentState: this.state,
-      prevState,
-      prevProps,
-    });
-  }
-
-  // Pega erros e info do erro
-  componentDidCatch(error, info) {
-    console.log('componentDidCatch ', { error, info });
-  }
-
-  // Executa antes da rerenderização do componente
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('shouldComponentUpdate ', {
-      currentState: this.state,
-      nextState,
-      nextProps,
-    });
-
-    // Return true se quiser que a rederização avance normalmente e false se quiser bloquera a renderização
-    return true;
-  }
-
   render() {
-    console.log('rendered');
     return (
       <ThemeProvider>
-        <button onClick={() => this.setState({ changed: true })}>
-          Change state
-        </button>
         <ThemeContext.Consumer>
           {({ theme }) => (
             <StyledThemeProvider theme={themes[theme] || themes.dark}>
               <GlobalStyle />
-              {theme === 'dark' && <Layout />}
-              <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+              <Layout />
             </StyledThemeProvider>
           )}
         </ThemeContext.Consumer>
